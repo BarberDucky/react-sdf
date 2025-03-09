63f8a88b25f63ce3e88b503ebf0b4b169f83ccbb
Damjan Trifunovic <damjan.trifunovic@gmail.com>
2025-03-09 10:04:31 +0100

    chore(utils): add dedent string tag function

--- src/utils.ts
+++ src/utils.ts
@@ -12,3 +12,29 @@ export function floatToGlslFloat(value: number): string {
 export function point3ToVec3(value: Point3) {
   return `vec3(${floatToGlslFloat(value.x)}, ${floatToGlslFloat(value.y)}, ${floatToGlslFloat(value.z)})`
 }
+
+export function dedent(strings: TemplateStringsArray, ...values: unknown[]): string {
+  let fullString = strings.reduce((acc, str, i) => {
+    const value = values[i] !== undefined ? String(values[i]) : ''
+    return acc + str + value
+  }, '')
+
+  const lines = fullString.split('\n')
+  while (lines.length && lines[0].trim() === '') lines.shift()
+  while (lines.length && lines[lines.length - 1].trim() === '') lines.pop()
+
+  const minIndent = lines.reduce((acc, line) => {
+    if (!line.trim()) return acc
+    const match = line.match(/^(\s+)/)
+    if (!match) return 0
+    return Math.min(acc, match[1].length)
+  }, Infinity)
+
+  if (minIndent !== Infinity) {
+    for (let i = 0; i < lines.length; i++) {
+      lines[i] = lines[i].slice(minIndent)
+    }
+  }
+
+  return lines.join('\n')
+}
